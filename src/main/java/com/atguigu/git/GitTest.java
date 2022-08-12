ccbb771f64d1c05230e557c0180ff4b8ba874c9b
wxd <wang_xiangdong@qq.com>
2022-08-12 11:55:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @title: GitTest
+ * @Author: Mrdong
+ * @Date: 2022/8/12 11:49
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+
+}
